ebf24cb257b901e5e1ffd942c6164f46465d6352
Rodrigo <rodrigosedano@me.com>
2022-12-26 18:59:14 +0100

    SteamOS Bug Warning

--- EmuDeck.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/EmuDeck/emudeck-we/main/EmuDeck_shell.ps1', 'EmuDeck_shell.ps1')"
-powershell -ExecutionPolicy RemoteSigned  -File EmuDeck_shell.ps1
-TIMEOUT /T 10
